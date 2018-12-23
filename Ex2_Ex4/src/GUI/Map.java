bda252a58c435230b8e2b2dcb9cb4b5794b90414
raam7777 <raam7777@gmail.com>
2018-12-23 22:54:54 +0200

    comnit last

--- Ex2_Ex4/src/GUI/Map.java
+++ Ex2_Ex4/src/GUI/Map.java
@@ -5,17 +5,82 @@ import org.omg.PortableServer.POA;
 import Geom.Point3D;
 
 
-public class Map {
-	int StartingWidth =1377;
-	int StartingHeiht = 642;
-	double TopLeftLon =35.2;
-	double TopLeftLat =32.1;
-	double bottomRightLon = 35.12-TopLeftLon;
-	double bottomRightLat = TopLeftLat- 32.06;
-	
-	public Map() {
-		
+public class Map 
+{
+
+	final int StartingWidth = 1433, StartingHeiht = 642;
+
+	final double TopLeftLon =35.20238  , TopLeftLat = 32.106046;
+
+	final double bottomRightLon = 35.21236-TopLeftLon; 
+
+	double bottomRightLat = TopLeftLat-32.101658;
+	Point3D RightDown = new Point3D(32.101073, 35.211922);
+	Point3D LeftUp    = new Point3D(32.106199,35.201708);
+
+	/**
+	 * default constructor
+	 */
+	public Map()
+	{
+
 	}
+	/**
+	 * default constructor 
+	 * @param StartingWidth - received width of the map
+	 * @param StartingHeiht - received height of the map
+	 */
+	public Map(int StartingWidth,int StartingHeiht)
+	{
+
+	}
+	/**
+	 * 
+	 * @param ctp - A point on the surface which will be received with coordinate values.
+	 * @return the value of the point - in pixel, calculated with a mathematical formula.
+	 */
+	public Point3D CtP(Point3D ctp)//The source code:https://stackoverflow.com/questions/38748832/convert-longitude-and-latitude-ctp-to-image-of-a-map-pixels-x-and-y-coor
+	{
+		double x,y;
+		x=ctp.y() - TopLeftLon;
+
+		y = TopLeftLat-ctp.x();
+
+		int x1 = (int) (StartingWidth*(x/bottomRightLon));
+		int y1 = (int) (StartingHeiht*(y/bottomRightLat));
+		if(x1<0) {
+			x1+=360;
+		}
+		if(y1<0) {
+			y1+=360;
+		}
+		return new Point3D(x1,y1);
+
+	}
+	/**
+	 * 
+	 * @param PointWidth - the X value of the point in pixels
+	 * @param PointHeight - the Y value of the point in pixels
+	 * @param widthMap - the width of the map we will receive
+	 * @param hightMap - the height of the map we will receive
+	 * @return - after making the convert with the requested calculations, we will return the point with suitable 
+	 *           coordinate values.
+	 */
+	public Point3D PtC(int PointWidth, int PointHeight,int widthMap,int hightMap) {
+		double coordsY = (((hightMap-(double)PointHeight)*RightDown.y())+((double)PointHeight*LeftUp.y()))/hightMap;
+		double coordsX = (((widthMap-(double)PointWidth)*RightDown.x())+((double)PointWidth*LeftUp.x()))/widthMap;
+		Point3D result =new Point3D(coordsX,coordsY);
+
+		return result;
+	}
+
+	/**
+	 * 
+	 * @param p1 - first point we will receive (GPS)
+	 * @param p2 - second point we will receive (GPS)
+	 * @return - distance in meters between the points 
+	 */
+
 	public  double pixelDistance(Point3D pixX, Point3D pixY) {
 		 int x1 =(int)CtP(pixX).x();
 		 int y1 =(int)CtP(pixX).y();
@@ -23,8 +88,14 @@ public class Map {
 		 int y2 =(int)CtP(pixY).y();
 		double res= Math.sqrt((Math.pow(x1-x2, 2))+(Math.pow(y1-y2, 2)));
 		return res;
-		
 	}
+	/**
+	 * 
+	 * @param point1 - first point we will receive (GPS)
+	 *
+	 * @param point2 - second point we will receive (GPS)
+	 * @return - The angle values located between the points   
+	 */
 	public  double pixelAngle(Point3D pixX, Point3D pixY) {
 		 int x1 =(int)CtP(pixX).x();
 		 int y1 =(int)CtP(pixX).y();
@@ -36,41 +107,6 @@ public class Map {
 			 
 		 }
 		 return rs;
-	}
-	
-	public  Point3D PtC(int pixelX, int pixelY) {// coverting pixel to coords - source code borought from stackoverflow
-		double latx =((pixelX/StartingWidth)*bottomRightLon);
-		double lony= ((pixelY/StartingHeiht)*bottomRightLat);
-		double coordsX =TopLeftLat-lony;
-		double coordsY= TopLeftLon + latx;
-		Point3D resault = new Point3D(coordsX,coordsY);
-		return resault;
-	}
-	public Point3D CtP(Point3D ctp) { // coverting from coords to pixel - source code borought from stackoverflow
-		
-		double x =ctp.y()-TopLeftLon; 
-		double y= TopLeftLat - ctp.x();
-		int x1 =(int) ((StartingWidth)*(x)/bottomRightLon);
-		int y1= (int) ((StartingHeiht)*(y)/bottomRightLat);
-		if(x1<0)
-			x1 = x1*(-1);
-		if(y1<0)
-			y1 = y1*(-1);
-		Point3D resault2 = new Point3D(x1,y1);
-		return resault2;
-	}
-	
-	
-	
-	
-	
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		Map m = new Map();
-		Point3D p = new Point3D(35.2,32.1);
-		System.out.println(m.CtP(p));
 	}
 
 }
